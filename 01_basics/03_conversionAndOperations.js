f250346f1c9c0108b458453a322197512f300640
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-07-11 23:15:00 +0530

    Add operations

--- 01_basics/03_conversionAndOperations.js
+++ 01_basics/03_conversionAndOperations.js
@@ -1,25 +1,25 @@
 let score ="50";
 
-console.log(typeof score); //output is string
-console.log(typeof (score));//output is string
+// console.log(typeof score); //output is string
+// console.log(typeof (score));//output is string
 
 //converting string to number
 let valuInScore = Number(score);
-console.log(typeof valuInScore); //output is number
-console.log(valuInScore + 10); //output is 60
+// console.log(typeof valuInScore); //output is number
+// console.log(valuInScore + 10); //output is 60
 
 let num ="50abc"
-console.log(typeof num);
+// console.log(typeof num);
 
 let newNum = Number(num);
-console.log(typeof newNum); 
+// console.log(typeof newNum); 
 
-console.log(newNum);
+// console.log(newNum);
 
 
 let number =null;
-console.log(typeof number); //output is object
-console.log(number); //output is null
+// console.log(typeof number); //output is object
+// console.log(number); //output is null
 
 //"33" => 33
 //"33abc" => NaN
@@ -37,5 +37,47 @@ let someNumber = 45;
 
 let stringNumber = String(someNumber)
 
-console.log(someNumber)
-console.log(typeof stringNumber)
+// console.log(someNumber)
+// console.log(typeof stringNumber)
+
+
+
+
+//***************************operations****************************
+
+let value = 3
+let negvalue =-value
+
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+
+let str1 ="helo";
+let str2 ="Rikesh"
+
+let str3 = str1 + str2;
+// console.log(str3);//output is  heloRikesh
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2 +2);//output is 122
+// console.log(1+2 +"2");//output is 32
+
+// console.log(+true)//output is 1
+console.log(+""); //output is 0
+
+ let  gameCounter =100;
+ gameCounter++;
+
+ console.log(gameCounter);
+
+
+
+
