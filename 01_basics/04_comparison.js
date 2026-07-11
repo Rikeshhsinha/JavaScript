be980ab08fd7e07ff4685b7826677ccf771559d0
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-07-11 23:45:06 +0530

    Add summary of data types

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1);//true
+// console.log(2>=1);//true
+// console.log(2<1);//false
+// console.log(2==1);//false
+// console.log(2 != 1);//true
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+//output is true because the js automatically convert the value from string to number
+//but many times it gives the unexpected results just like this 
+
+//example
+
+// console.log(null > 0); //false
+// console.log(null == 0);//false
+// console.log(null >= 0);//true unexpected
+
+// === strict check
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+
+
