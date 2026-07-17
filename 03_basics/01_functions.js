7a87554afc4fde4a66692666cff0f1e429351996
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-07-17 23:47:04 +0530

    Add function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,17 @@
+
+function  sayMyName(){
+
+    console.log('Rikesh');
+    
+}
+
+sayMyName //referance
+// sayMyName() //function execution 
+
+function addTwoNum(num1,num2){
+
+    console.log(num1 + num2);
+    
+}
+
+addTwoNum(2,3)
