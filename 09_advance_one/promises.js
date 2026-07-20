152020db4cf30cac26be306a72cdab732bac2e50
Rikesh Kumar <rikeshsinha245@gmail.com>
2026-07-20 13:42:38 +0530

    Add async await in promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -41,3 +41,93 @@ promiseThree.then(function(user){
     
 
 })
+
+
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+
+        let error = false;
+        if(!error){
+            resolve({username:'rikesh',passward:'1234'});
+        }     
+            else{
+                reject('ERROR : something went wrong')
+            }
+        
+    },1000)
+})
+
+ promiseFour.then((user)=>{
+    console.log(user);
+
+    return user.username
+    
+}).then((username)=>{
+
+    console.log(username)
+}).catch(function(error){
+    console.log(error);
+    
+}).finally(function(){
+    console.log("The promise is either resolved or rejected");
+    
+})
+
+
+
+
+const promiseFive = new Promise(function(resolve,reject){
+
+
+    setTimeout(function(){
+        let error =true;
+        if(!error){
+            resolve({username:'Gaurav',passward:'1234'});
+        }     
+            else{
+                reject('ERROR : something went wrong')
+            }
+
+        
+    },1000);
+
+})
+
+
+async function consumePromiseFive(){
+
+      try {
+        const response = await promiseFive
+      console.log(response);
+      
+      } catch (error) {
+        console.log("error");
+        
+        
+      }
+
+} 
+
+consumePromiseFive();
+
+
+// async function getAllUser() {
+//    try {
+//     const responce = await fetch('https://jsonplaceholder.typicode.com/users');
+
+//    const data = await responce.json()
+//     console.log(data);
+    
+//    } catch (error) {
+//     console.log("Error");
+    
+//    }
+// }
+
+// getAllUser();
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then()
+.catch()
